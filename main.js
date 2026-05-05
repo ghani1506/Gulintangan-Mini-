3e3f9bdd8ef24b0e351ca0f7abf13769884b8164
ghani1506 <147093492+ghani1506@users.noreply.github.com>
2026-05-05 13:15:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-Modify this and replace the sound respectively with caklempong sound with the same notes
-
 // Ultra-fast, polyphonic WebAudio. Prebuilt AudioBuffers; immediate .start(). No labels, fixed image.
 const NOTES = {
   'C4':261.63, 'C#4':277.18, 'D4':293.66, 'D#4':311.13, 'E4':329.63,
